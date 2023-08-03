8c3bbefacb4cbe9f515b8d7bbc5c33fbbbe29d5b
elliseggs11 <jellis@companieshouse.gov.uk>
2023-08-03 14:18:48 +0100

    remove ingresses

--- groups/cvo/security_group_rules.tf
+++ groups/cvo/security_group_rules.tf
@@ -26,19 +26,6 @@ resource "aws_security_group_rule" "onpremise" {
   cidr_blocks = var.client_ips
 }
 
-resource "aws_security_group_rule" "onpremise_cvo2" {
-  for_each = { for rule in var.client_ports : rule.port => rule }
-
-  security_group_id = module.cvo2.cvo_security_group_id
-  description       = "Allow on premise ranges to access CVO2"
-
-  type        = "ingress"
-  from_port   = each.value.port
-  to_port     = lookup(each.value, "to_port", each.value.port)
-  protocol    = each.value.protocol
-  cidr_blocks = var.client_ips
-}
-
 resource "aws_security_group_rule" "onpremise_icmp" {
 
   security_group_id = module.cvo.cvo_security_group_id
@@ -51,33 +38,10 @@ resource "aws_security_group_rule" "onpremise_icmp" {
   cidr_blocks = var.client_ips_icmp
 }
 
-resource "aws_security_group_rule" "onpremise_icmp_cvo2" {
-
-  security_group_id = module.cvo2.cvo_security_group_id
-  description       = "Allow only the on premise NetApp metro-cluster range to ping CVO2 via ICMP"
-
-  type        = "ingress"
-  from_port   = "-1"
-  to_port     = "-1"
-  protocol    = "icmp"
-  cidr_blocks = var.client_ips_icmp
-}
-
 resource "aws_security_group_rule" "onpremise_admin" {
 
   security_group_id = module.cvo.cvo_security_group_id
   description       = "Allow on-premise ranges to access CVO over SSH and HTTPS for administration"
-  for_each          = toset(["22", "443"])
-  type              = "ingress"
-  from_port         = each.value
-  to_port           = each.value
-  protocol          = "tcp"
-  cidr_blocks       = local.admin_cidrs
-}
-resource "aws_security_group_rule" "onpremise_admin_cvo2" {
-
-  security_group_id = module.cvo2.cvo_security_group_id
-  description       = "Allow on-premise ranges to access CVO2 over SSH and HTTPS for administration"
   for_each          = toset(["22", "443"])
   type              = "ingress"
   from_port         = each.value
@@ -98,20 +62,6 @@ resource "aws_security_group_rule" "cardiff_nfs_cifs" {
   protocol    = each.value.protocol
   cidr_blocks = var.nfs_cifs_cidrs
 }
-
-resource "aws_security_group_rule" "cardiff_nfs_cifs_cvo2" {
-  for_each = { for rule in var.nfs_cifs_ports : join("_", [rule.protocol, rule.port]) => rule }
-
-  security_group_id = module.cvo2.cvo_security_group_id
-  description       = "Allow Cardiff Backend range to access CVO2 via NFS and CIFS"
-
-  type        = "ingress"
-  from_port   = each.value.port
-  to_port     = lookup(each.value, "to_port", each.value.port)
-  protocol    = each.value.protocol
-  cidr_blocks = var.nfs_cifs_cidrs
-}
-
 # ------------------------------------------------------------------------------
 # NFS and CIFS
 # ------------------------------------------------------------------------------
