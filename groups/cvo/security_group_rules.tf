7983a5f1d0e9be964d88b1a5767284e1e7a2b96e
elliseggs11 <jellis@companieshouse.gov.uk>
2023-09-21 09:52:31 +0100

    code commented out in old cvo

--- groups/cvo/security_group_rules.tf
+++ groups/cvo/security_group_rules.tf
@@ -63,12 +63,12 @@ resource "aws_security_group_rule" "cardiff_nfs_cifs" {
 # ------------------------------------------------------------------------------
 # NFS and CIFS
 # ------------------------------------------------------------------------------
-# data "aws_network_interfaces" "cvo_data_eni" {
-#   filter {
-#     name   = "group-id"
-#     values = [module.cvo.cvo_security_group_id]
-#   }
-# }
+data "aws_network_interfaces" "cvo_data_eni" {
+  filter {
+    name   = "group-id"
+    values = [module.cvo.cvo_security_group_id]
+  }
+}
 
 # ------------------------------------------------------------------------------
 # Dedciated NFS Client Access Rules
