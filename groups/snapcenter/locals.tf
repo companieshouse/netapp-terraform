95ef6af2e0e57a6425282253cd7b6610bb1fc6df
lee-mk-joseph <ljoseph@companieshouse.gov.uk>
2023-11-21 15:11:59 +0000

    Implement looping for udp & tcp azure sgs

--- groups/snapcenter/locals.tf
+++ groups/snapcenter/locals.tf
@@ -2,49 +2,48 @@ locals {
   ec2_secret_data = data.vault_generic_secret.ec2_data.data
   kms_secret_data = data.vault_generic_secret.kms_data.data
 
-  # azure_ad_ingress_rules_tcp_udp = merge(
-  #   [
-  #     for idx, ports in var.azure_ad_ingress_tcp_udp :
-  #     {
-  #       for port in ports :
-  #       "${idx}-${port}" => {
-  #         "port"  = port
-  #         "cidrs" = var.azure_ad_cidrs[idx]
-  #       }
-  #     }
-  #   ]
-  # )
-
-  # azure_ad_ingress_rules_tcp = merge(
-  #   [
-  #     for idx, ports in var.azure_ad_ingress_tcp :
-  #     {
-  #       for port in ports :
-  #       "${idx}-${port}" => {
-  #         "port"  = port
-  #         "cidrs" = var.azure_ad_cidrs[idx]
-  #       }
-  #     }
-  #   ]
-  # )
-
-  azure_ad_ingress_rules_udp = [
-      for idx in var.azure_ad_cidrs :
+  azure_ad_ingress_rules_tcp_udp = merge(
+    [
+      for idx, ports in var.azure_ad_ingress_tcp_udp :
       {
-        for port in var.azure_ad_ingress_udp:
+        for port in ports :
         "${idx}-${port}" => {
           "port"  = port
-          "cidrs" = idx
+          "cidrs" = var.azure_ad_cidrs[idx]
         }
       }
-    ]
+    ]...)
+
+  azure_ad_ingress_rules_tcp = merge(
+    [
+      for idx, ports in var.azure_ad_ingress_tcp:
+      {
+        for port in ports :
+        "${idx}-${port}" => {
+          "port"  = port
+          "cidrs" = var.azure_ad_cidrs[idx]
+        }
+      }
+    ]...)
+
+  azure_ad_ingress_rules_udp = merge(
+    [
+      for idx, ports in var.azure_ad_ingress_udp:
+        {
+          for port in ports:
+            "${idx}-${port}" => {
+              "port" = port
+              "cidrs" = var.azure_ad_cidrs[idx]
+              }
+        }
+    ]...)
   
-  Azure_ad_ingress_port_ranges_tcp_udp = [
-    { from_port = 3268, to_port = 3269, protocol = "tcp", cidr_blocks = var.azure_ad_cidrs[0] },
-    { from_port = 3268, to_port = 3269, protocol = "tcp", cidr_blocks = var.azure_ad_cidrs[1] },
-    { from_port = 137, to_port = 138, protocol = "udp", cidr_blocks = var.azure_ad_cidrs[0] },
-    { from_port = 137, to_port = 138, protocol = "udp", cidr_blocks = var.azure_ad_cidrs[1] },
-    { from_port = 49152, to_port = 65535, protocol = "-1", cidr_blocks = var.azure_ad_cidrs[0] },
-    { from_port = 49152, to_port = 65535, protocol = "-1", cidr_blocks = var.azure_ad_cidrs[1] }
-  ]
+#  Azure_ad_ingress_port_ranges_tcp_udp = [
+#    { from_port = 3268, to_port = 3269, protocol = "tcp", cidr_blocks = var.azure_ad_cidrs[0] },
+#    { from_port = 3268, to_port = 3269, protocol = "tcp", cidr_blocks = var.azure_ad_cidrs[1] },
+#    { from_port = 137, to_port = 138, protocol = "udp", cidr_blocks = var.azure_ad_cidrs[0] },
+#    { from_port = 137, to_port = 138, protocol = "udp", cidr_blocks = var.azure_ad_cidrs[1] },
+#    { from_port = 49152, to_port = 65535, protocol = "-1", cidr_blocks = var.azure_ad_cidrs[0] },
+#    { from_port = 49152, to_port = 65535, protocol = "-1", cidr_blocks = var.azure_ad_cidrs[1] }
+#  ]
 }
--- groups/snapcenter/security_group_rules.tf
+++ groups/snapcenter/security_group_rules.tf
@@ -1,41 +1,28 @@
-# resource "aws_security_group_rule" "azure_ad_tcp_udp" {
-#   type              = "ingress"
-#   for_each          = local.azure_ad_ingress_rules_tcp_udp
-#   description       = "Allow Azure AD"
-#   from_port         = each.value.port
-#   to_port           = each.value.port
-#   protocol          = "-1"
-#   cidr_blocks       = each.value.cidrs
-#   security_group_id = aws_security_group.snapcenter.id
-# }
+resource "aws_security_group_rule" "azure_ad_tcp_udp" {
+  type              = "ingress"
+  for_each          = local.azure_ad_ingress_rules_tcp_udp
+  description       = "Allow Azure AD"
+  from_port         = each.value.port
+  to_port           = each.value.port
+  protocol          = "-1"
+  cidr_blocks       = each.value.cidrs
+  security_group_id = aws_security_group.snapcenter.id
+}
 
-# resource "aws_security_group_rule" "azure_ad_tcp" {
-#   type              = "ingress"
-#   for_each          = local.azure_ad_ingress_rules_tcp
-#   description       = "Allow Azure AD"
-#   from_port         = each.value.port
-#   to_port           = each.value.port
-#   protocol          = "tcp"
-#   cidr_blocks       = each.value.cidrs
-#   security_group_id = aws_security_group.snapcenter.id
-# }
-
-# resource "aws_security_group_rule" "azure_ad_udp" {
-#   type              = "ingress"
-#   for_each          = local.azure_ad_ingress_rules_udp
-#   description       = "Allow Azure AD"
-#   from_port         = each.value.port
-#   to_port           = each.value.port
-#   protocol          = "udp"
-#   cidr_blocks       = each.value.cidrs
-#   security_group_id = aws_security_group.snapcenter.id
-# }
+resource "aws_security_group_rule" "azure_ad_tcp" {
+  type              = "ingress"
+  for_each          = local.azure_ad_ingress_rules_tcp
+  description       = "Allow Azure AD"
+  from_port         = each.value.port
+  to_port           = each.value.port
+  protocol          = "tcp"
+  cidr_blocks       = each.value.cidrs
+  security_group_id = aws_security_group.snapcenter.id
+}
 
 resource "aws_security_group_rule" "azure_ad_udp" {
   type              = "ingress"
-  for_each    = {
-     for n in local.azure_ad_ingress_rules_udp : n.port => n
-   }
+  for_each          = local.azure_ad_ingress_rules_udp
   description       = "Allow Azure AD"
   from_port         = each.value.port
   to_port           = each.value.port
@@ -44,6 +31,14 @@ resource "aws_security_group_rule" "azure_ad_udp" {
   security_group_id = aws_security_group.snapcenter.id
 }
 
-output "test"{
+output "test_udp"{
     value = local.azure_ad_ingress_rules_udp
+}
+
+output "test_tcp"{
+    value = local.azure_ad_ingress_rules_tcp
+}
+
+output "test_udp_tcp"{
+    value = local.azure_ad_ingress_rules_tcp_udp
 }
