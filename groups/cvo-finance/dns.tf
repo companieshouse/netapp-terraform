1650aff1dba72b3addd77e74f162645ff0e80cdc
Chris Harris <charris1@companieshouse.gov.uk>
2024-10-21 13:48:12 +0100

    removed indicies

--- groups/cvo-finance/dns.tf
+++ groups/cvo-finance/dns.tf
@@ -12,18 +12,18 @@ resource "aws_route53_record" "cvo_data_n1_dns" {
   count = var.cvo_multi_node ? 1 : 0
 
   zone_id = data.aws_route53_zone.private_zone.id
-  name    = var.cvo_data_n1_dns_name[0]
+  name    = var.cvo_data_n1_dns_name
   type    = "A"
   ttl     = 300
-  records = [var.cvo_data_n1_dns_record[0]]
+  records = [var.cvo_data_n1_dns_record]
 }
 
 resource "aws_route53_record" "cvo_data_n2_dns" {
   count = var.cvo_multi_node ? 1 : 0
 
   zone_id = data.aws_route53_zone.private_zone.id
-  name    = var.cvo_data_n2_dns_name[0]
+  name    = var.cvo_data_n2_dns_name
   type    = "A"
   ttl     = 300
-  records = [var.cvo_data_n2_dns_record[0]]
+  records = [var.cvo_data_n2_dns_record]
 }
