ccfa6a47d4418d1c53a4d790a9e4b38ee0db9201
elliseggs11 <jellis@companieshouse.gov.uk>
2025-04-22 13:39:44 +0100

    add iboss_cidr variable

--- groups/unifiedmanager/locals.tf
+++ groups/unifiedmanager/locals.tf
@@ -5,7 +5,7 @@ locals {
   account_ids          = data.vault_generic_secret.account_ids.data
   admin_cidrs          = values(data.vault_generic_secret.internal_cidrs.data)
   unified_manager_data = data.vault_generic_secret.unified_manager.data
-  iboss_cidr           = data.aws_ec2_managed_prefix_list.iboss["iBoss VPN"]
+  iboss_cidr           = [for entry in data.aws_ec2_managed_prefix_list.iboss.entries : entry.cidr if entry.description == "iBoss VPN" ]
 
   internal_fqdn = "${replace(var.aws_account, "-", "")}.aws.internal"
 
