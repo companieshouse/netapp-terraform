22d6d5390ec7e8de4511a2b31b5f39021961a655
ls-comh <172278859+ls-comh@users.noreply.github.com>
2025-09-15 14:39:44 +0100

    Re-add new line

--- groups/snapcenter-linux/cloudwatch.tf
+++ groups/snapcenter-linux/cloudwatch.tf
@@ -2,7 +2,8 @@ resource "aws_cloudwatch_log_group" "snapcenter_linux" {
   name              = "/aws/ec2/${local.common_resource_name}"
   retention_in_days = var.default_log_retention_in_days
   kms_key_id        = local.cloudwatch_logs_kms_key
-  tags              = local.common_tags
+
+  tags = local.common_tags
 }
 
 ########## Instance Status Check ###############################################################
