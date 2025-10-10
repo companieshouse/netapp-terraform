85fd849ae536cac604d0bfeabcdd7d6f164b3731
ls-comh <172278859+ls-comh@users.noreply.github.com>
2025-10-10 13:48:52 +0100

    Add shared-services cidrs

--- groups/snapcenter-linux/security_groups.tf
+++ groups/snapcenter-linux/security_groups.tf
@@ -18,6 +18,15 @@ resource "aws_vpc_security_group_ingress_rule" "snapcenter_ssh_admin" {
   to_port           = 22
 }
 
+resource "aws_vpc_security_group_ingress_rule" "snapcenter_ssh_shared_services" {
+  description       = "Allow SSH connectivity for ansible (via pipelines)"
+  security_group_id = aws_security_group.snapcenter_linux.id
+  prefix_list_id    = data.aws_ec2_managed_prefix_list.shared_services_build_cidr_ranges.id
+  ip_protocol       = "tcp"
+  from_port         = 22
+  to_port           = 22
+}
+
 # SnapCenter Ports
 resource "aws_vpc_security_group_ingress_rule" "snapcenter_main_port" {
   description       = "SnapCenter main communication port (web UI and API)"
